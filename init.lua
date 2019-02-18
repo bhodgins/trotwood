a4cccaa46b77a250ad0efd38a0d3361b0588cbe6
Brian Hodgins <bhodgins@300-baud.net>
2019-02-18 09:36:02 -0500

    ocfs boot updates

--- init.lua
+++ init.lua
@@ -1,14 +1,42 @@
 -- init.lua - OCFS boot compatability for Trotwood
 
+local ocfs = component.proxy(computer.getBootAddress())
+
 local function send(co, ...)
   -- TODO
 end
 
-local function require()
+--[[ slurp :: Loads an entire file into a string
+type:       Internal
+params:     fh:number, acc:string
+returns:    file:string ]]
+local function slurp(fh, acc)
+  local content = ocfs.read(fh, 4096)
+  if content ~= nil then return slurp(fh, acc .. content) end
+  return acc
+end
+
+--[[ require :: requires a Lua source file
+type:       Implied
+params:     file:string
+returns:    any ]]
+function require(file)
+  local fh      = ocfs.open(file)
+  local content = slurp(fh, "")
+  local result, err = pcall(content)
+  error(err, 2)
+  assert(result, error)
+
+  return result
+end
+
+local kernel = require('system/kernel.lua')
 
 while true do
   coroutine.yield()
 end
+
+--[[
 
 local ocfs_disk_ns = 'ocfs|' .. ocfs_disk_uuid
 send(vfs, -1, ':mount', ocfs_disk_ns, ocfs_pid)
@@ -17,3 +45,5 @@ send(vfs, -1, ':mount', ocfs_disk_ns, ocfs_pid)
 send(vfs, -1, ':bind', 'system', ocfs_disk_ns .. ':///system/$u')
 send(vfs, -1, ':bind', 'boot', ocfs_disk_ns .. ':///boot/$u')
 send(vfs, -1, ':bind', 'lib', ocfs_disk_ns .. ':///lib/$u')
+
+--]]
