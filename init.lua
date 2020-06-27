bf02c130c788cb20698b1f7c449a9d4dc0cc6ef2
Brian Hodgins <bhodgins@300-baud.net>
2020-06-27 12:17:39 -0400

    error test on OC

--- init.lua
+++ init.lua
@@ -10,5 +10,5 @@ if _SYSTEM == "UNKNOWN" then stub = require('stub/standard') end
 
 local core = stub.require('system://core')
 local initial_state = core.build(1000)
-core.spawn(initial_state, "while true do cprint('Hello, World I am a new process, my pid is: ' .. _SELF .. ' and I am running on target system: ' .. _SYSTEM .. '!') coroutine.yield() end")
+core.spawn(initial_state, "while true do _cprint('Hello, World I am a new process, my pid is: ' .. _SELF .. ' and I am running on target system: ' .. _SYSTEM .. '!') coroutine.yield() end")
 core.run(initial_state)
