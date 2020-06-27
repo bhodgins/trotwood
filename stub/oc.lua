f11291530c542b61c52e56fb12ee6ddb7368231e
Brian Hodgins <bhodgins@300-baud.net>
2020-06-27 12:39:58 -0400

    OS stub improvements

--- stub/oc.lua
+++ stub/oc.lua
@@ -3,4 +3,17 @@ local _S = {}
 if cprint == nil then cprint = function() end end	-- OC compat
 function _S.get_event() return table.pack(computer.pullSignal(0)) or {} end
 
+if _OSVERSION ~= nil then
+    if _string.find(_OSVERSION, "OpenOS") ~= nil then
+        -- Filesystem is just like on standard stub and we expect to use managed disks:
+        function _S.require(resource)
+            local _d = resource:find('://')
+            local namespace = resource:sub(1, _d - 1)
+            local path      = resource:sub(_d + 3)
+        
+            return require(namespace .. '/' .. path)
+        end
+    end
+end
+
 return _S
